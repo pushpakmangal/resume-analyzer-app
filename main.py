54a6554978402252e8d3308c9f6a272b8a128eda
Pushpak Mangal <119566845+pushpakmangal@users.noreply.github.com>
2025-02-16 21:49:03 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -16,26 +16,28 @@ except:
 import boto3
 from botocore.exceptions import ClientError
 
-# Set up the DynamoDB client using environment variables
-aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-aws_region = os.environ.get('AWS_REGION', 'ap-south-1')
+try:
+    # Set up the DynamoDB client using environment variables
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    aws_region = os.environ.get('AWS_REGION', 'ap-south-1')
 
-dynamodb = boto3.resource('dynamodb',
-                          region_name=aws_region,
-                          aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key)
+    dynamodb = boto3.resource('dynamodb',
+                            region_name=aws_region,
+                            aws_access_key_id=aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key)
 
-def upload_item_to_dynamodb(table_name, item):
-    table = dynamodb.Table(table_name)
-    
-    try:
-        response = table.put_item(Item=item)
-        print(f"Item uploaded successfully: {response}")
-    except ClientError as e:
-        print(f"Error uploading item: {e.response['Error']['Message']}")
+    def upload_item_to_dynamodb(table_name, item):
+        table = dynamodb.Table(table_name)
+        
+        try:
+            response = table.put_item(Item=item)
+            print(f"Item uploaded successfully: {response}")
+        except ClientError as e:
+            print(f"Error uploading item: {e.response['Error']['Message']}")
 
-
+except Exception as _:
+    pass
 
 # Configure Streamlit page
 st.set_page_config(
@@ -170,8 +172,11 @@ def main():
                         'response': analysis.split('</think>')[1]
                     }
 
-                    upload_item_to_dynamodb(table_name, item)
-
+                    try:
+                        upload_item_to_dynamodb(table_name, item)
+                    except Exception as _:
+                        pass
+                    
                     if analysis:
                         st.markdown(f"""<div class="analysis-box-think"><h2>Thinking</h2>{analysis.split('</think>')[0]}</div> 
                         <div class="analysis-box-result"><h2>Response</h2>{analysis.split('</think>')[1]}</div>""", 
